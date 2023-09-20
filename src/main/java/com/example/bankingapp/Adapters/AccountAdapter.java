504816fd0cdfa5832831ab2ee1f0d3e70e285af5
Sentinelle Technologies <96134432+SentinelleTech@users.noreply.github.com>
2023-09-20 09:49:18 +0300

    Add files via upload

---
+++ src/main/java/com/example/bankingapp/Adapters/AccountAdapter.java
@@ -0,0 +1,62 @@
+package com.example.bankingapp.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.bankingapp.Model.Account;
+import com.example.bankingapp.R;
+
+import java.util.ArrayList;
+
+
+
+public class AccountAdapter extends ArrayAdapter<Account> {
+
+    private Context context;
+    private int resource;
+
+    public AccountAdapter(Context context, int resource, ArrayList<Account> accounts) {
+        super(context, resource, accounts);
+
+        this.context = context;
+        this.resource = resource;
+    }
+
+    /**
+     * function that gets the view from the adapter
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return
+     */
+    @Override
+    @NonNull
+    public View getView (int position, View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null) {
+
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            convertView = inflater.inflate(resource, parent, false);
+        }
+
+        Account account = getItem(position);
+
+        TextView txtAccountName = convertView.findViewById(R.id.txt_account_name);
+        txtAccountName.setText(account.getAccountName());
+
+        TextView txtAccountNo = convertView.findViewById(R.id.txt_acc_no);
+        txtAccountNo.setText(context.getString(R.string.account_no) + " " + account.getAccountNo());
+
+        TextView txtAccountBalance = convertView.findViewById(R.id.txt_balance);
+        txtAccountBalance.setText("Account balance: $" + String.format("%.2f",account.getAccountBalance()));
+
+        return convertView;
+    }
+}
